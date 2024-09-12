471dfe36fc6b0a039520701462568cf64b65f626
Oleh Yemelianov <emelianov2811@gmail.com>
2024-09-12 20:13:44 +0300

    Task 4 completed

--- child.py
+++ child.py
@@ -1,1 +1,29 @@
-class Base
+class Base:
+    def __init__(self, greeting):
+        self.greeting = greeting
+
+    @classmethod
+    def method(cls, greeting):
+        cls.greeting = greeting
+
+    def __str__(self):
+        return f'{self.greeting}'
+
+
+class Child:
+    def __init__(self, greeting):
+        self.greeting = greeting
+
+    @classmethod
+    def method(cls, greeting):
+        cls.greeting = greeting
+
+    def __str__(self):
+        return f'{self.greeting}'
+
+
+base1 = Base('Hello from base')
+child1 = Child('Hello from child')
+
+print(base1)
+print(child1)
